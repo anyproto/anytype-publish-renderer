a949ecb2a27de95482e6403320b2d270ca2c111a
Anatolii Smolianinov <zarkonesmall@gmail.com>
2024-12-17 14:19:32 +0100

    fix tests for new format

--- renderer/renderer_test.go
+++ renderer/renderer_test.go
@@ -1,47 +1,35 @@
 package renderer
 
 import (
-	"os"
-
-	"github.com/anyproto/anytype-publish-renderer/cmd/resolver"
 	"go.uber.org/zap"
 )
 
-var testRenderRoots map[string]string
-var testRenderers map[string]*Renderer
+var (
+	testRenderers = make(map[string]*Renderer)
+)
 
-func makeTestRenderer(id, rootId string) *Renderer {
-	resolver := resolver.SimpleAssetResolver{
-		CdnUrl:      "http://test-cdn",
-		SnapshotDir: "../test_snapshots/" + id,
-		RootPageId:  rootId,
+func makeTestRenderer(dir string) *Renderer {
+	config := RenderConfig{
+		StaticFilesPath:  "/static",
+		PublishFilesPath: "../test_snapshots/" + dir,
+		PrismJsCdnUrl:    "https://cdn.jsdelivr.net/npm/prismjs@1.29.0",
+		AnytypeCdnUrl:    "https://anytype-static.fra1.cdn.digitaloceanspaces.com",
+		AnalyticsCode:    `<script>console.log("sending dummy analytics...")</script>`,
 	}
 
-	r, err := NewRenderer(resolver, os.Stdout)
+	r, err := NewRenderer(config)
+
 	if err != nil {
 		log.Fatal("failed to make test renderer", zap.Error(err))
 	}
 
 	return r
 }
-func makeTestRenderRoots() {
-	if testRenderRoots == nil {
-		testRenderRoots = map[string]string{
-			"snapshot_pb":  "bafyreiftcdiken5kayp3x4ix7tm4okmizhyshev3jjl5r2jjenz2d5uwui",
-			"snapshot_pb2": "bafyreiecs2ivic6ne2lvkohrf3ojqizngmsk5ywilexo6xhtzlqfgngp64",
-			"snapshot_pb3": "bafyreiecs2ivic6ne2lvkohrf3ojqizngmsk5ywilexo6xhtzlqfgngp64",
-		}
-	}
-}
-func getTestRenderer(id string) *Renderer {
-	makeTestRenderRoots()
-	if testRenderers == nil {
-		testRenderers = make(map[string]*Renderer, 0)
+
+func getTestRenderer(dir string) *Renderer {
+	if _, ok := testRenderers[dir]; !ok {
+		testRenderers[dir] = makeTestRenderer(dir)
 	}
 
-	if _, ok := testRenderers[id]; !ok {
-		rootId := testRenderRoots[id]
-		testRenderers[id] = makeTestRenderer(id, rootId)
-	}
-	return testRenderers[id]
+	return testRenderers[dir]
 }
