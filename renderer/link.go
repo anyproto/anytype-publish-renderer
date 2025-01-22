ed423cc7499c8d9b6cb848152efa39ab56e3098b
AnastasiaShemyakinskaya <shem98a@mail.ru>
2025-01-22 20:37:07 +0100

    GO-4889: refactoring
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- renderer/link.go
+++ renderer/link.go
@@ -5,7 +5,6 @@ import (
 	"github.com/anyproto/anytype-heart/pkg/lib/bundle"
 	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
 	"github.com/gogo/protobuf/types"
-	"go.uber.org/zap"
 )
 
 const linkTemplate = "anytype://object?objectId=%s&spaceId=%s"
@@ -26,150 +25,25 @@ type LinkRenderParams struct {
 	CoverParams   *CoverRenderParams
 }
 
-func (r *Renderer) MakeLinkRenderParams(b *model.Block) (params *LinkRenderParams) {
+func (r *Renderer) MakeLinkRenderParams(b *model.Block) *LinkRenderParams {
 	targetObjectId := b.GetLink().GetTargetBlockId()
-	var targetObjectIdDetails *types.Struct
-	for _, detail := range r.Sp.GetDependantDetails() {
-		if detail.Id == targetObjectId {
-			targetObjectIdDetails = detail.Details
-			break
-		}
-	}
-	if targetObjectIdDetails == nil || len(targetObjectIdDetails.Fields) == 0 {
+	targetDetails := r.findTargetDetails(targetObjectId)
+	if targetDetails == nil || len(targetDetails.Fields) == 0 {
 		return &LinkRenderParams{IsDeleted: true}
 	}
-	var linkTypeClass string
-	if b.GetLink().GetCardStyle() == model.BlockContentLink_Card {
-		linkTypeClass = "card"
-	}
-	if b.GetLink().GetCardStyle() == model.BlockContentLink_Text {
-		linkTypeClass = "text"
-	}
-	var (
-		description      string
-		descriptionValue *types.Value
-	)
-	if b.GetLink().GetDescription() == model.BlockContentLink_Content {
-		descriptionValue = targetObjectIdDetails.GetFields()[bundle.RelationKeySnippet.String()]
-	}
-	if b.GetLink().GetDescription() == model.BlockContentLink_Added {
-		descriptionValue = targetObjectIdDetails.GetFields()[bundle.RelationKeyDescription.String()]
-	}
-	if descriptionValue != nil {
-		description = descriptionValue.GetStringValue()
-	}
 
-	deletedValue := targetObjectIdDetails.GetFields()[bundle.RelationKeyIsDeleted.String()]
-	if deletedValue != nil && deletedValue.GetBoolValue() {
+	linkTypeClass := getLinkTypeClass(b)
+	description := getDescription(b, targetDetails)
+	if isDeleted(targetDetails) {
 		return &LinkRenderParams{IsDeleted: true}
 	}
-	name := targetObjectIdDetails.GetFields()[bundle.RelationKeyName.String()].GetStringValue()
-	if name == "" {
-		name = defaultName
-	}
-	var iconStyle, icon string
-	iconClass := "c20"
-	if b.GetLink().GetIconSize() != model.BlockContentLink_SizeNone && b.GetLink().GetCardStyle() == model.BlockContentLink_Text {
-		iconStyle = "smileImage c20"
-	}
-	if b.GetLink().GetCardStyle() == model.BlockContentLink_Card {
-		if b.GetLink().GetIconSize() == model.BlockContentLink_SizeMedium {
-			iconStyle = "smileImage c28"
-			iconClass = "c48"
-		}
-		if b.GetLink().GetIconSize() == model.BlockContentLink_SizeSmall {
-			iconStyle = "smileImage c20"
-		}
-	}
-	layout := model.ObjectTypeLayout(targetObjectIdDetails.GetFields()[bundle.RelationKeyLayout.String()].GetNumberValue())
 
-	var layoutClass string
-	switch layout {
-	case model.ObjectType_participant:
-		layoutClass = "isParticipant"
-	case model.ObjectType_profile:
-		layoutClass = "isHuman"
-	case model.ObjectType_todo:
-		layoutClass = "isTask"
-	case model.ObjectType_collection:
-		layoutClass = "isCollection"
-	case model.ObjectType_set:
-		layoutClass = "isSet"
-	default:
-		layoutClass = "isPage"
-	}
+	name := getFieldValue(targetDetails, bundle.RelationKeyName.String(), defaultName)
+	icon, iconClass, iconStyle := r.getIconParams(b, targetDetails)
+	layoutClass := getLayoutClass(targetDetails)
+	archiveClass := getArchiveClass(targetDetails)
 
-	if b.GetLink().GetIconSize() != model.BlockContentLink_SizeNone {
-		if layout == model.ObjectType_todo {
-			iconStyle = "iconCheckbox c20 icon checkbox unset"
-			doneValue := targetObjectIdDetails.GetFields()[bundle.RelationKeyDone.String()]
-			if doneValue != nil && doneValue.GetBoolValue() {
-				iconStyle = "iconCheckbox c20 icon checkbox set"
-			}
-		} else {
-			iconValue := targetObjectIdDetails.GetFields()[bundle.RelationKeyIconEmoji.String()]
-			if iconValue != nil && iconValue.GetStringValue() != "" {
-				emojiRune := []rune(iconValue.GetStringValue())[0]
-				icon = r.GetEmojiUrl(emojiRune)
-				iconClass = iconClass + " withIcon"
-			}
-			if iconValue == nil || iconValue.GetStringValue() == "" {
-				iconValue = targetObjectIdDetails.GetFields()[bundle.RelationKeyIconImage.String()]
-				if iconValue == nil || iconValue.GetStringValue() == "" {
-					iconSize := "c20"
-					if b.GetLink().GetIconSize() == model.BlockContentLink_SizeMedium {
-						iconSize = "c28"
-					}
-					if layout == model.ObjectType_collection || layout == model.ObjectType_set {
-						iconStyle = "iconCommon icon collection " + iconSize
-					}
-					if layout != model.ObjectType_note && layout != model.ObjectType_profile && layout != model.ObjectType_participant {
-						iconStyle = "iconCommon icon page " + iconSize
-					}
-				}
-				var err error
-				icon, err = r.getFileUrl(icon)
-				if err != nil {
-					log.Error("failed to get file url", zap.Error(err))
-				} else {
-					iconClass = iconClass + " withImage"
-				}
-			}
-		}
-	}
-
-	archivedValue := targetObjectIdDetails.GetFields()[bundle.RelationKeyIsArchived.String()]
-
-	var archiveClass string
-	if archivedValue != nil && archivedValue.GetBoolValue() {
-		archiveClass = "isArchived"
-	}
-
-	var (
-		objectTypeName string
-		coverParams    *CoverRenderParams
-		err            error
-		coverClass     string
-	)
-	for _, relation := range b.GetLink().GetRelations() {
-		if relation == bundle.RelationKeyType.String() {
-			objectType := targetObjectIdDetails.GetFields()[bundle.RelationKeyType.String()].GetStringValue()
-			for _, detail := range r.Sp.GetDependantDetails() {
-				if detail.Id == objectType {
-					objectTypeName = detail.Details.GetFields()[bundle.RelationKeyName.String()].GetStringValue()
-					break
-				}
-			}
-		}
-		if relation == "cover" {
-			coverParams, err = r.getCoverParams(targetObjectIdDetails)
-			if err != nil {
-				log.Error("failed to get cover params", zap.Error(err))
-			} else {
-				coverClass = "withCover"
-			}
-		}
-	}
+	objectTypeName, coverParams, coverClass := r.getAdditionalParams(b, targetDetails)
 
 	return &LinkRenderParams{
 		Id:            b.GetId(),
@@ -185,6 +59,154 @@ func (r *Renderer) MakeLinkRenderParams(b *model.Block) (params *LinkRenderParam
 		CoverClass:    coverClass,
 		CoverParams:   coverParams,
 	}
+}
+
+func (r *Renderer) findTargetDetails(targetObjectId string) *types.Struct {
+	for _, detail := range r.Sp.GetDependantDetails() {
+		if detail.Id == targetObjectId {
+			return detail.Details
+		}
+	}
+	return nil
+}
+
+func getLinkTypeClass(b *model.Block) string {
+	switch b.GetLink().GetCardStyle() {
+	case model.BlockContentLink_Card:
+		return "card"
+	default:
+		return "text"
+	}
+}
+
+func getDescription(b *model.Block, details *types.Struct) string {
+	var key string
+	switch b.GetLink().GetDescription() {
+	case model.BlockContentLink_Content:
+		key = bundle.RelationKeySnippet.String()
+	case model.BlockContentLink_Added:
+		key = bundle.RelationKeyDescription.String()
+	default:
+		return ""
+	}
+
+	descriptionValue := details.GetFields()[key]
+	if descriptionValue != nil {
+		return descriptionValue.GetStringValue()
+	}
+	return ""
+}
+
+func isDeleted(details *types.Struct) bool {
+	deletedValue := details.GetFields()[bundle.RelationKeyIsDeleted.String()]
+	return deletedValue != nil && deletedValue.GetBoolValue()
+}
+
+func getFieldValue(details *types.Struct, key, defaultValue string) string {
+	value := details.GetFields()[key]
+	if value == nil || value.GetStringValue() == "" {
+		return defaultValue
+	}
+	return value.GetStringValue()
+}
+
+func getLayoutClass(details *types.Struct) string {
+	layout := model.ObjectTypeLayout(details.GetFields()[bundle.RelationKeyLayout.String()].GetNumberValue())
+	switch layout {
+	case model.ObjectType_participant:
+		return "isParticipant"
+	case model.ObjectType_profile:
+		return "isHuman"
+	case model.ObjectType_todo:
+		return "isTask"
+	case model.ObjectType_collection:
+		return "isCollection"
+	case model.ObjectType_set:
+		return "isSet"
+	default:
+		return "isPage"
+	}
+}
+
+func getArchiveClass(details *types.Struct) string {
+	archivedValue := details.GetFields()[bundle.RelationKeyIsArchived.String()]
+	if archivedValue != nil && archivedValue.GetBoolValue() {
+		return "isArchived"
+	}
+	return ""
+}
+
+func (r *Renderer) getIconParams(b *model.Block, details *types.Struct) (icon, iconClass, iconStyle string) {
+	iconClass = "c20"
+	if b.GetLink().GetIconSize() == model.BlockContentLink_SizeNone {
+		return
+	}
+	layout := model.ObjectTypeLayout(details.GetFields()[bundle.RelationKeyLayout.String()].GetNumberValue())
+	if layout == model.ObjectType_todo && b.GetLink().GetIconSize() != model.BlockContentLink_SizeNone {
+		iconStyle = "iconCheckbox c20 icon checkbox unset"
+		if doneValue := details.GetFields()[bundle.RelationKeyDone.String()]; doneValue != nil && doneValue.GetBoolValue() {
+			iconStyle = "iconCheckbox c20 icon checkbox set"
+		}
+		return
+	}
+	if b.GetLink().GetIconSize() != model.BlockContentLink_SizeNone {
+		iconStyle = "smileImage c20"
+	}
+	if b.GetLink().GetCardStyle() == model.BlockContentLink_Card {
+		if b.GetLink().GetIconSize() == model.BlockContentLink_SizeMedium {
+			iconStyle = "smileImage c28"
+			iconClass = "c48"
+		} else if b.GetLink().GetIconSize() == model.BlockContentLink_SizeSmall {
+			iconStyle = "smileImage c20"
+		}
+	}
+	iconValue := details.GetFields()[bundle.RelationKeyIconEmoji.String()]
+	if iconValue != nil && iconValue.GetStringValue() != "" {
+		emojiRune := []rune(iconValue.GetStringValue())[0]
+		icon = r.GetEmojiUrl(emojiRune)
+		iconClass += " withIcon"
+	}
+	if iconValue == nil || iconValue.GetStringValue() == "" {
+		iconValue = details.GetFields()[bundle.RelationKeyIconImage.String()]
+		if iconValue != nil && iconValue.GetStringValue() != "" {
+			icon, _ = r.getFileUrl(iconValue.GetStringValue())
+			iconClass += " withImage"
+		}
+	}
+	if icon == "" && b.GetLink().GetIconSize() != model.BlockContentLink_SizeNone {
+		iconSize := "c20"
+		if b.GetLink().GetIconSize() == model.BlockContentLink_SizeMedium {
+			iconSize = "c28"
+		}
+		if layout == model.ObjectType_collection || layout == model.ObjectType_set {
+			iconStyle = "iconCommon icon collection " + iconSize
+		} else if layout != model.ObjectType_note && layout != model.ObjectType_profile && layout != model.ObjectType_participant {
+			iconStyle = "iconCommon icon page " + iconSize
+		}
+	}
+	return
+}
+
+func (r *Renderer) getAdditionalParams(b *model.Block, details *types.Struct) (objectTypeName string, coverParams *CoverRenderParams, coverClass string) {
+	for _, relation := range b.GetLink().GetRelations() {
+		if relation == bundle.RelationKeyType.String() {
+			objectType := details.GetFields()[bundle.RelationKeyType.String()].GetStringValue()
+			for _, detail := range r.Sp.GetDependantDetails() {
+				if detail.Id == objectType {
+					objectTypeName = detail.Details.GetFields()[bundle.RelationKeyName.String()].GetStringValue()
+					break
+				}
+			}
+		}
+		if relation == "cover" {
+			var err error
+			coverParams, err = r.getCoverParams(details)
+			if err == nil {
+				coverClass = "withCover"
+			}
+		}
+	}
+	return
 }
 
 func (r *Renderer) RenderLink(b *model.Block) templ.Component {
